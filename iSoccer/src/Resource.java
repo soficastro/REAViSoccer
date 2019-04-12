cc6766de1c224ef9d233376e3a13b481d34e9596
soficastro <38478765+soficastro@users.noreply.github.com>
2019-04-11 21:13:44 -0300

    Update Resource.java

--- iSoccer/src/Resource.java
+++ iSoccer/src/Resource.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Resource {
 
     private boolean available;
@@ -8,6 +10,28 @@ public class Resource {
 
     public boolean isAvailable() {
         return available;
+    }
+
+    public static void resourcesReport (Systema system) {
+        System.out.println("Vehicles:");
+
+        if (system.vehicles.isEmpty()) System.out.println("Error");
+        else {
+            for (Bus object : system.vehicles) {
+                System.out.println(object.toString());
+            }
+        }
+
+        System.out.println("Stadium:");
+        if(system.stadium.isAvailable()) {
+            System.out.println( system.stadium.toString());
+        } else System.out.println("Not available:");
+
+        System.out.println("Training center:");
+        if(system.trainingCenter.isAvailable()) {
+            System.out.println( system.trainingCenter.toString());
+        } else System.out.println("Not available:");
+
     }
 
     public void setAvailable(boolean available) {
