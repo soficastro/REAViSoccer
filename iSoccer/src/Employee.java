43d4abcd1da85c917ed0b0b2904a07e875d4d4fc
soficastro <38478765+soficastro@users.noreply.github.com>
2019-04-11 21:11:53 -0300

    Update Employee.java

--- iSoccer/src/Employee.java
+++ iSoccer/src/Employee.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Employee extends Person {
     private int salary;
@@ -16,6 +17,59 @@ public class Employee extends Person {
         this.salary = salary;
     }
 
+
+    public static void employeeReport (Systema system) {
+        System.out.println("Team:");
+        System.out.println("Coach:");
+        system.coach.toString();
+        if (system.players.isEmpty()) System.out.println("Error");
+        else {
+            System.out.println("Team players:");
+            for (Player object : system.players) {
+                    System.out.println(object.toString());
+                }
+        }
+        System.out.println("Service:");
+        System.out.println("President:");
+        System.out.println( system.president.toString());
+        System.out.println("Doctor:");
+        System.out.println( system.doctor.toString());
+        if (system.lawyers.isEmpty()) System.out.println("Error");
+        else {
+            System.out.println("Lawyers:");
+            for (Employee object : system.lawyers) {
+                System.out.println(object.toString());
+            }
+        }
+        if (system.personalTrainers.isEmpty()) System.out.println("Error");
+        else {
+            System.out.println("Personal Trainers:");
+            for (Employee object : system.personalTrainers) {
+                System.out.println(object.toString());
+            }
+        }
+        if (system.cooks.isEmpty()) System.out.println("Error");
+        else {
+            System.out.println("Cooks:");
+            for (Employee object : system.cooks) {
+                System.out.println(object.toString());
+            }
+        }
+        if (system.drivers.isEmpty()) System.out.println("Error");
+        else {
+            System.out.println("Drivers:");
+            for (Rider object : system.drivers) {
+                System.out.println(object.toString());
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Salary: $" + this.salary + "\n";
+    }
+
     public static class EmployeeBuilder extends PersonBuilder {
         int salary;
 
@@ -31,17 +85,17 @@ public class Employee extends Person {
 
         @Override
         public Employee.EmployeeBuilder setEmail(String email) {
-            return (Employee.EmployeeBuilder)super.setName(email);
+            return (Employee.EmployeeBuilder)super.setEmail(email);
         }
 
         @Override
         public Employee.EmployeeBuilder setCpf(String cpf) {
-            return (Employee.EmployeeBuilder)super.setName(cpf);
+            return (Employee.EmployeeBuilder)super.setCpf(cpf);
         }
 
         @Override
         public Employee.EmployeeBuilder setTelephone(String telephone) {
-            return (Employee.EmployeeBuilder)super.setName(telephone);
+            return (Employee.EmployeeBuilder)super.setTelephone(telephone);
         }
 
         public Employee build(){
